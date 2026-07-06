4289d279bff4cfa4724f70540f6252428ffa4580
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "eventhub_clusters" {
   description = "All eventhub_cluster resources"
   value       = azurerm_eventhub_cluster.eventhub_clusters
 }
+output "eventhub_clusters_location" {
+  description = "List of location values across all eventhub_clusters"
+  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.location]
+}
+output "eventhub_clusters_name" {
+  description = "List of name values across all eventhub_clusters"
+  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.name]
+}
+output "eventhub_clusters_resource_group_name" {
+  description = "List of resource_group_name values across all eventhub_clusters"
+  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.resource_group_name]
+}
+output "eventhub_clusters_sku_name" {
+  description = "List of sku_name values across all eventhub_clusters"
+  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.sku_name]
+}
+output "eventhub_clusters_tags" {
+  description = "List of tags values across all eventhub_clusters"
+  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.tags]
+}
 
