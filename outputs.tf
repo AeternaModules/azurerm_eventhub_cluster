084044d2c9e66d56ccc01dab17df66adc4af1d68
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "eventhub_clusters" {
-  description = "All eventhub_cluster resources"
-  value       = azurerm_eventhub_cluster.eventhub_clusters
-}
 output "eventhub_clusters_location" {
-  description = "List of location values across all eventhub_clusters"
-  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.location]
+  description = "Map of location values across all eventhub_clusters, keyed the same as var.eventhub_clusters"
+  value       = { for k, v in azurerm_eventhub_cluster.eventhub_clusters : k => v.location }
 }
 output "eventhub_clusters_name" {
-  description = "List of name values across all eventhub_clusters"
-  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.name]
+  description = "Map of name values across all eventhub_clusters, keyed the same as var.eventhub_clusters"
+  value       = { for k, v in azurerm_eventhub_cluster.eventhub_clusters : k => v.name }
 }
 output "eventhub_clusters_resource_group_name" {
-  description = "List of resource_group_name values across all eventhub_clusters"
-  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.resource_group_name]
+  description = "Map of resource_group_name values across all eventhub_clusters, keyed the same as var.eventhub_clusters"
+  value       = { for k, v in azurerm_eventhub_cluster.eventhub_clusters : k => v.resource_group_name }
 }
 output "eventhub_clusters_sku_name" {
-  description = "List of sku_name values across all eventhub_clusters"
-  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.sku_name]
+  description = "Map of sku_name values across all eventhub_clusters, keyed the same as var.eventhub_clusters"
+  value       = { for k, v in azurerm_eventhub_cluster.eventhub_clusters : k => v.sku_name }
 }
 output "eventhub_clusters_tags" {
-  description = "List of tags values across all eventhub_clusters"
-  value       = [for k, v in azurerm_eventhub_cluster.eventhub_clusters : v.tags]
+  description = "Map of tags values across all eventhub_clusters, keyed the same as var.eventhub_clusters"
+  value       = { for k, v in azurerm_eventhub_cluster.eventhub_clusters : k => v.tags }
 }
 
